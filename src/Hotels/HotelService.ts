e542770fc67789cc6c1a02dc4ac6cd9024d83c6d
helena6421 <40118858+helena6421@users.noreply.github.com>
2025-03-16 19:09:09 +0300

    модуль отеля

--- src/Hotels/HotelService.ts
+++ src/Hotels/HotelService.ts
@@ -1,38 +1,36 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-
 import { CreateHotelDto } from "./Dtos/CreateHotelDto";
+import { UpdateHotelDto } from "./Dtos/UpdateHotelDto";
 import { Hotel, HotelDocument } from "./HotelSchema";
-import { IHotelService } from "./Interfaces/HotelInterface";
+import { IHotelService, SearchHotelParams } from "./Interfaces/HotelInterface";
 
 @Injectable()
 export class HotelService implements IHotelService {
   constructor(
     @InjectModel(Hotel.name) private hotelModel: Model<HotelDocument>
   ) {}
-
   create(createHotelDto: CreateHotelDto): Promise<Hotel> {
     const createdHotel = new this.hotelModel(createHotelDto);
     return createdHotel.save().then(({ id }) => {
       return this.hotelModel.findById(id, "title description");
     });
   }
-
   findById(id: string): Promise<Hotel> {
     return this.hotelModel.findById(id).exec();
   }
-
-  search(params: Pick<Hotel, "title">): Promise<Hotel[]> {
+  search(params: SearchHotelParams): Promise<Hotel[]> {
     return this.hotelModel
-      .find(
-        { title: { $regex: new RegExp(params.title, "i") } },
-        "title description"
-      )
+      .find({}, "title description")
+      .limit(+params.limit)
+      .skip(+params.offset)
       .exec();
   }
-
-  update(id: string, data: Partial<Hotel>): Promise<Hotel> {
-    return this.hotelModel.findByIdAndUpdate(id, data).exec();
+  update(id: string, data: UpdateHotelDto): Promise<Hotel> {
+    return this.hotelModel
+      .findByIdAndUpdate(id, data, { new: true })
+      .select({ title: 1, description: 1 })
+      .exec();
   }
 }
